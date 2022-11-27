e615d912c04dc1bd15306d38414723325226b792
Akshata Bedarkar <akshatabedarkar@gmail.com>
2022-11-27 22:23:53 +0530

    CustomHelper

---
+++ CustomHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CustomHelper
+{
+    public static class CustomHelper
+    {
+        public static IHtmlString Image(string src,string alt)
+        {
+            return new MvcHtmlString(string.Format("<img src='{0}' alt='{1}'></img>",src,alt));
+        }
+
+        public static IHtmlString Imaging(this HtmlHelper helper,string src, string alt)
+        {
+            return new MvcHtmlString(string.Format("<img src='{0}' alt='{1}'></img>", src, alt));
+        }
+
+        public static IHtmlString ImgTag(this HtmlHelper helper, string src, string alt)
+        {
+            TagBuilder tag = new TagBuilder("img");
+            tag.Attributes.Add("src", src);
+            tag.Attributes.Add("alt", alt);
+            return new MvcHtmlString(tag.ToString());
+        }
+    }
+}
